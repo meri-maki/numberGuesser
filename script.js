1630c9dc1b0e4d79cfa58395d97367bc349141da
Ana Prahova <anastasiaprahova@gmail.com>
2022-07-05 15:17:12 +0300

    added files

---
+++ script.js
@@ -0,0 +1,32 @@
+let humanScore = 6;
+let computerScore = 2;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}; 
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+   // Shorthand if statement because results are boolean
+  return humanDifference <= computerDifference;
+}
+ 
+  
+const checkHumanGuess = (humanGuess) => {
+    if(humanGuess < 0 || humanGuess > 9){ 
+        window.alert("Please make sure your guess is between 0 and 9");
+    }
+};
+
+  const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
